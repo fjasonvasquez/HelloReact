bc89b9364e543a431b45fbbc9087800629bb9308
fjasonvasquez <fjvasquez@aol.com>
2017-01-28 15:26:52 -0800

    created nav component

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-	entry: './public/app.jsx',
+	entry: './app/app.jsx',
 	output: {
 		path: __dirname,
 		filename: './public/bundle.js'
 	},
 	resolve: {
+		root: __dirname,
+		alias: {
+			Main: 'app/components/Main.jsx',
+			Nav: 'app/components/Nav.jsx'
+		},
 		extensions: ['', '.js', '.jsx']
 	},
 	module: {
@@ -12,7 +17,7 @@ module.exports = {
 			{
 				loader: 'babel-loader',
 				query: {
-					presets: ['react', 'es2015']
+					presets: ['react', 'es2015', 'stage-0']
 				},
 				test: /\.jsx?$/,
 				exclude: /(node_modules|bower_components)/
